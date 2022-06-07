1d0ab03bb98007307b262ec9580f3f1a5b976e85
arthur <arthur.f@gec.inatel.br>
2022-06-07 14:39:43 -0300

    código ADC

--- E209_LAB/aula_seila_qual/src/main.cpp
+++ E209_LAB/aula_seila_qual/src/main.cpp
@@ -1,8 +1,22 @@
 #include <Arduino.h>
 
 int main(){
+  DDRD = 1<<PD5;
+
+  TCCR0A = 0b10000001;
+  TCCR0B = (1<<CS00);
+  TCNT0 = 0;
+  OCR0A = 0;
+
+  ADMUX = (1<<REFS0);
+  ADCSRA = 0b10000111;
+
+  while(1){
+    ADCSRA |= (1<<ADSC);
+
+    while(ADCSRA == (ADCSRA | (1<<ADSC))){
+      OCR0A = (ADC*1023)/255;
+    }
+  }
   
-
-
-
 }
